302063aab66ce8ec57cc559fb11568f7d8f9ec64
Joe Rezendes <rezenbiz@gmail.com>
2018-01-09 15:30:25 -0800

    Just got mongoose up and running with the finished create handle

---
+++ controllers/posts.js
@@ -0,0 +1,28 @@
+var Post = require('../models/post');
+
+module.exports = (app) => {
+
+  // CREATE
+  app.post('/posts/new', (req, res) => {
+    // INSTANTIATE INSTANCE OF POST MODEL
+    var post = new Post(req.body);
+    console.log("------------------------")
+    console.log(post);
+    post.save().then((post) => {
+      console.log("Post saved!")
+      res.redirect(`/`);
+    }).catch((err) => {
+      console.log(err.message)
+    })
+    // SAVE INSTANCE OF POST MODEL TO DB
+    // Post.create((err, post) => {
+    //   if (err) {
+    //     return(err)
+    //   }
+    //
+    //   // REDIRECT TO THE ROOT
+    //    res.redirect(`/`);
+    // })
+  });
+
+};
--- server.js
+++ server.js
@@ -1,13 +1,29 @@
 const express = require('express');
 const app = express();
+const mongoose = require('mongoose');
+const bodyParser = require('body-parser');
 
 
 
 var exphbs = require('express-handlebars');
+app.use(bodyParser.json());
+app.use(bodyParser.urlencoded({extended: true}));
+app.use(express.static('public'));
 
 app.engine('handlebars', exphbs({defaultLayout: 'main'}));
 app.set('view engine', 'handlebars');
-app.use(express.static('public'));
+
+
+const posts = require('./controllers/posts.js')(app);
+
+
+
+mongoose.Promise = global.Promise
+mongoose.connect('mongodb://localhost/redditclone', { useMongoClient : true });
+console.log("You are connected to the db")
+
+let db = mongoose.connection;
+db.on('error', console.error.bind(console, 'connection error:'));
 
 
 
